bcb1d5abf7c4e7bb4f81a235cdb7943498e99005
nikita9876 <nikitanikvta@gmail.com>
2016-09-22 23:25:06 +0300

    first commit

--- access-app.js
+++ access-app.js
@@ -1,14 +1,35 @@
-(function() {
-  angular.module('accessApp', [])
-    .controller('AccessCtrl', AccessCtrl);
-    function AccessCtrl($scope) {
-      $scope.users = [
-        {name:'Paul Marshall', rights:'read'},
-        {name:'Andy Owens', rights:'read'},
-        {name:'Arnold Morris', rights:'write'},
-        {name:'Stacy Martin', rights:'admin'}
-      ];
 
-    };
+  var app = angular.module('accessApp', []);
+    app.controller('AccessCtrl', AccessCtrl);
+    function AccessCtrl(UsersService) {
+      $scope.users = UsersService.getAll();
+  });
 
-})();
+  app.directive('Users', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+        user: '='
+      },
+      templateUrl: 'users.html'
+    }
+  });
+
+
+    app.service('UsersService', function($http) {
+        var users = [
+          {name:'Paul Marshall', rights:'read'},
+          {name:'Andy Owens', rights:'read'},
+          {name:'Arnold Morris', rights:'write'},
+          {name:'Stacy Martin', rights:'admin'}
+        ];
+        this.getOne = function(id) {
+          return users[id];
+        };
+
+        this.getAll = function() {
+          return users;
+        };
+
+      });
