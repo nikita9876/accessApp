db5368f8cfe1e2c4535d804250e4e853bbe7f0f3
Nikita Pogrebnoi <nikitanikvta@gmail.com>
2016-09-26 17:36:55 +0300

    fourth commit

--- access-app.js
+++ access-app.js
@@ -5,11 +5,32 @@ var app = angular.module('accessApp', []);
           {name:'Michelle Hopkins', rights:'read'},
           {name:'Dave Kelley', rights:'read'}
       ];
-      $scope.users = UsersService.getAll();
+      $scope.variations = [
+          {right: 'Read'},
+          {right: 'Write'},
+          {right: 'Admin'}
 
-      $scope.delete = function(user) {
-        $scope.users.splice($scope.users.indexOf(user), 1);
-        };
+      ];
+      $scope.users = UsersService.getAll();
+        
+      $scope.delete = UsersService.delete;
+
+      $scope.addUser = function() {
+          var found = false;
+          angular.forEach($scope.users, function(val){
+             if($scope.search === val.name){
+                 found = true;
+
+             }
+          });
+
+          if (!found) {
+              $scope.users.push({
+                  name: $scope.search,
+                  rights: $scope.access
+              })
+          }
+      };
   });
 
   app.directive('users',function() {
@@ -26,16 +47,20 @@ var app = angular.module('accessApp', []);
 
   });
 
-    app.service('UsersService', function($http) {
-        var users = [
-          {name:'Paul Marshall', rights:'read'},
-          {name:'Andy Owens', rights:'read'},
-          {name:'Arnold Morris', rights:'write'},
-          {name:'Stacy Martin', rights:'admin'}
-        ];
+  app.service('UsersService', function($http) {
+      var users = [
+        {name:'Paul Marshall', rights:'Read'},
+        {name:'Andy Owens', rights:'Read'},
+        {name:'Arnold Morris', rights:'Write'},
+        {name:'Stacy Martin', rights:'Admin'}
+      ];
 
-        this.getAll = function() {
-          return users;
+      this.getAll = function() {
+        return users;
+      };
+
+        this.delete = function(user) {
+            this.users.splice(this.users.indexOf(user), 1);
         };
 
       });
