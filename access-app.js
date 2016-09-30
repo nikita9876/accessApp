bcda699df1a0ca9425e51b246ecc92f1932cf9d1
Nikita Pogrebnoi <nikitanikvta@gmail.com>
2016-09-30 17:53:03 +0300

    fifth commit

--- access-app.js
+++ access-app.js
@@ -1,79 +1,89 @@
 var app = angular.module('accessApp', []);
-    app.controller('AccessCtrl', function ($scope, UsersService) {
-      $scope.others = [
-          {name:'Dylan Fernandez', rights:'read'},
-          {name:'Michelle Hopkins', rights:'read'},
-          {name:'Dave Kelley', rights:'read'}
-      ];
+app.controller('AccessCtrl', function ($scope, UsersService) {
+    $scope.access = 'Read';
+    $scope.others = [
+        {name: 'Dylan Fernandez'},
+        {name: 'Michelle Hopkins'},
+        {name: 'Dave Kelley'}
+    ];
 
-      $scope.select = [
-          {right: 'Admin'},
-          {right: 'Write'},
-          {right: 'Read'}
+    $scope.options = [
+        {rights: 'Read'},
+        {rights: 'Write'},
+        {rights: 'Admin'}
+    ];
 
-      ];
+    $scope.users = UsersService.getUsers();
 
-      $scope.users = UsersService.getUsers();
+    $scope.delete = UsersService.delete;
 
-      $scope.delete = UsersService.delete;
+    $scope.addUser = function () {
+        UsersService.addUser($scope.search, $scope.access);
+    };
 
-      $scope.addUser = function() {
-          UsersService.addUser($scope.search, $scope.access);
-      }
+    // $scope.change = function () {
+    //   $scope.access = ;
+    // };
+
+    $scope.$watch('access', function () {
+        console.log($scope.access);
     });
+});
 
 
-  app.directive('users',function() {
+app.directive('users', function () {
     return {
-      restrict: 'E',
-      replace: true,
-      scope: {
-        users: '='
-      },
-      templateUrl: 'users.html',
+        restrict: 'E',
+        replace: true,
+        scope: {
+            users: '='
+        },
+        templateUrl: 'users.html',
         controller: 'AccessCtrl'
 
-    }
+    };
 
-  });
+});
 
-  app.service('UsersService', function() {
-      this.saved = localStorage.getItem('users');
-      this.users = (localStorage.getItem('users')!==null) ? JSON.parse(this.saved) : [
-          {name:'Paul Marshall', rights:'Read'},
-          {name:'Andy Owens', rights:'Read'},
-          {name:'Arnold Morris', rights:'Write'},
-          {name:'Stacy Martin', rights:'Admin'}
-      ];
-      localStorage.setItem('users', JSON.stringify(this.users));
+app.service('UsersService', function () {
+    this.saved = localStorage.getItem('users');
+    this.users = (localStorage.getItem('users') !== null) ? JSON.parse(this.saved) : [
+        {name: 'Paul Marshall', rights: 'Read'},
+        {name: 'Andy Owens', rights: 'Read'},
+        {name: 'Arnold Morris', rights: 'Write'},
+        {name: 'Stacy Martin', rights: 'Admin'}
+    ];
+    localStorage.setItem('users', JSON.stringify(this.users));
 
-      this.getUsers = function() {
-          return this.users;
-      };
+    this.getUsers = function () {
+        return this.users;
+    };
 
-      this.delete = function(user) {
-          this.users.splice(this.users.indexOf(user), 1);
-          localStorage.setItem('users', JSON.stringify(this.users));
-      };
+    this.delete = function (user) {
+        this.users.splice(this.users.indexOf(user), 1);
+        localStorage.setItem('users', JSON.stringify(this.users));
+    };
 
-      this.addUser = function(search, access) {
-          var found = false;
-          angular.forEach(this.users, function(val){
-              if(search === val.name){
-                  found = true;
+    this.addUser = function (search, access) {
+        var found = false;
+        angular.forEach(this.users, function (val) {
+            if (search === val.name) {
+                found = true;
 
-              }
-          });
+            }
+        });
 
-          if (!found) {
-              this.users.push({
-                  name: search,
-                  rights: access
-              })
-          }
-          localStorage.setItem('users', JSON.stringify(this.users));
+        if (!found) {
+            this.users.push({
+                name: search,
+                rights: access
+            });
+            console.log({
+                name: search,
+                rights: access
+            })
+        }
+        localStorage.setItem('users', JSON.stringify(this.users));
 
-      };
-      });
-
-
+    };
+});
